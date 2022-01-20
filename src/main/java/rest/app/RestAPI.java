f45b6fed962b259032c9300c873b210c7ef65640
Etienne BEGON* <etienne.begon@fr.thalesgroup.com>
2022-01-20 11:45:19 +0100

    Add rabbit

--- src/main/java/rest/app/RestAPI.java
+++ src/main/java/rest/app/RestAPI.java
@@ -1,14 +1,36 @@
 package rest.app;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("api")
-public class RestAPI extends ResourceConfig {
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
-	public RestAPI() {
+@ApplicationPath("api")
+@WebListener
+public class RestAPI extends ResourceConfig implements ServletContextListener {
+
+	private final static String QUEUE_NAME = "hello";
+
+	public RestAPI() throws IOException, TimeoutException {
 		packages("resource");
+		System.out.println("hallo");
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("rabbit.begon.dev");
+		try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			String message = "Test 3!";
+			channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+			System.out.println(" [x] Sent '" + message + "'");
+		}
 	}
 
 }
